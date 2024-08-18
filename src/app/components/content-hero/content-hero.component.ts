d5b00fa834b19ee315e03b3fb7d8599f56473f96
Tiranthine <22277108+Tiranthine@users.noreply.github.com>
2024-08-18 21:36:38 +0200

    Dynamified Hero Scroll from cover

--- src/app/components/content-hero/content-hero.component.ts
+++ src/app/components/content-hero/content-hero.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'comp-contentHero',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './content-hero.component.html',
   styleUrl: './content-hero.component.scss'
 })
 export class ContentHeroComponent {
+
+  @Input() title!: string;
+  @Input() content!: string;
+  @Input() imageSrc?: string;
+  @Input() imageAlt?: string;
+  @Input() imagePos: "left" | "right" = "right";
+  @Input() button1Text?: string;
+  @Input() button1Href?: string;
+  @Input() button1Target: "_self" | "_blank" = "_self";
+  @Input() button2Text?: string;
+  @Input() button2Href?: string;
+  @Input() button2Target: "_self" | "_blank" = "_self";
 
 }
