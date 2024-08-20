fac8dd56ecb47d1ac01d22d6d9632527b6f6f165
Lena | Dunkel <7890309+DarkView@users.noreply.github.com>
2024-08-20 17:37:35 +0200

    Add clipshow video as background on landing

--- src/app/pages/mainPage/mainPage.component.ts
+++ src/app/pages/mainPage/mainPage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../../components/navBar/navBar.component';
 import { ContentBlockComponent } from '../../components/contentBlock/contentBlock.component';
@@ -7,14 +7,41 @@ import { CoverComponent } from '../../components/coverComponent/coverComponent.c
 import { ContentHeroComponent } from '../../components/content-hero/content-hero.component';
 import { ContentDividerComponent } from '../../components/content-divider/content-divider.component';
 import { FeaturesComponent } from '../../components/features/features.component';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'page-MainPage',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, ContentBlockComponent, FooterBarComponent, CoverComponent, ContentHeroComponent, ContentDividerComponent, FeaturesComponent],
+  imports: [RouterOutlet, NavBarComponent, ContentBlockComponent, FooterBarComponent, CoverComponent, ContentHeroComponent, ContentDividerComponent, FeaturesComponent, CommonModule],
   templateUrl: './mainPage.component.html',
-  styleUrl: './mainPage.component.scss'
+  styleUrl: './mainPage.component.scss',
+  animations: [ 
+    trigger('navFade', [
+      transition(':enter',
+        [style({ 'opacity': '0' }),
+        animate(250, style({ 'opacity': 1 }))]
+      ),
+      transition(':leave',
+        [style({ 'opacity': '1' }),
+        animate(250, style({ 'opacity': 0 }))]
+      )
+    ])
+  ]
 })
 export class MainPageComponent {
   title = 'spectra-website';
+
+  scrolled = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event: any) {
+    const numb = window.scrollY;
+    if (numb >= 125){
+      this.scrolled = 1;
+    }
+    else {
+      this.scrolled = 0;
+    }
+  }
 }
