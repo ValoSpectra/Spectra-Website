991f5d57fbba3836e3b59c7a7a2c20b9ca07bc91
Lena | Dunkel <7890309+DarkView@users.noreply.github.com>
2024-08-24 23:28:00 +0200

    Add demo page, more text to landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/mainPage/mainPage.component';
 import { CreditsPageComponent } from './pages/creditsPage/creditsPage.component';
 import { NgModule } from '@angular/core';
+import { LiveDemoPageComponent } from './pages/liveDemoPage/liveDemoPage.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,14 @@ export const routes: Routes = [
         data: {
             title: "Spectra - Credits",
             description: "Everyone who has contributed to Spectra in one way or another. Developers, Designers, and more."
+        }
+    },
+    {
+        path: "live-demo",
+        component: LiveDemoPageComponent,
+        data: {
+            title: "Spectra - Live Demo",
+            description: "Learn more about the live demo for Spectra - created to test out Spectra functionality in a simple and intuitive way."
         }
     }
 ];
---
+++ src/app/pages/liveDemoPage/liveDemoPage.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from '../../components/navBar/navBar.component';
+import { ContentBlockComponent } from '../../components/contentBlock/contentBlock.component';
+import { FooterBarComponent } from '../../components/footerBar/footerBar.component';
+import { ContentDividerComponent } from '../../components/content-divider/content-divider.component';
+import { ContentHeroComponent } from '../../components/content-hero/content-hero.component';
+
+@Component({
+  selector: 'page-LiveDemoPage',
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent, ContentBlockComponent, FooterBarComponent, ContentDividerComponent, ContentHeroComponent],
+  templateUrl: './liveDemoPage.component.html',
+  styleUrl: './liveDemoPage.component.scss'
+})
+export class LiveDemoPageComponent {
+  
+}
